f6cc9442e6049639b085f704320040bb8a6083d5
MadhuriDipankar <98409073+MadhuriDipankar@users.noreply.github.com>
2022-02-03 19:38:44 +0530

    Update HelloController.java
    Postmapping added

--- src/main/java/com/example/azure/spring/azure/demo/controller/HelloController.java
+++ src/main/java/com/example/azure/spring/azure/demo/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.example.azure.spring.azure.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	@GetMapping (value = "/welcome")
+	@PostMapping (value = "/index")
 	public String wish()
 	{     
 		return "Hello from Spring boot to Azure...Thanks ";
