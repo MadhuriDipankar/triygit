0d87cdf829e7a16cdc4a1173607e515ff3d601f6
MadhuriDipankar <98409073+MadhuriDipankar@users.noreply.github.com>
2022-02-04 10:06:12 +0530

    Update Application.java
    adding controller in main method....

--- src/main/java/com/example/azure/spring/azure/demo/Application.java
+++ src/main/java/com/example/azure/spring/azure/demo/Application.java
@@ -2,9 +2,18 @@ package com.example.azure.spring.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
+	
+
+@RestController
+@GetMapping (value = "/")
+	public String wish()
+	{     
+		return "Hello from Spring boot to Azure...Thanks ";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
