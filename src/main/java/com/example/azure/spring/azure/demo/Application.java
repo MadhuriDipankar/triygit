9b470d82189582caa6dda76bbfe970e212c72f20
MadhuriDipankar <98409073+MadhuriDipankar@users.noreply.github.com>
2022-02-04 10:10:06 +0530

    Update Application.java
    undo changes

--- src/main/java/com/example/azure/spring/azure/demo/Application.java
+++ src/main/java/com/example/azure/spring/azure/demo/Application.java
@@ -2,20 +2,13 @@ package com.example.azure.spring.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class Application {
 	
 
-@RestController
-@GetMapping (value = "/")
-	public String wish()
-	{     
-		return "Hello from Spring boot to Azure...Thanks ";
-	}
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
